616df8cffa3ca0d76f222b8545e54288dff94ec7
steffFrank <--replace-all>
2022-06-22 21:59:11 +0200

    refactor: Use a for of instead of forEach

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Javascript logic 
+// Javascript logic
 
 //Animation of the button
 
@@ -9,29 +9,28 @@ let links = document.querySelectorAll(".header-nav__link");
 let toggle = false;
 
 menu.addEventListener("click", () => {
-    // Remove the no-animation class after the click;
-    bars.forEach(bar => bar.classList.remove("menu__bar--no-animation"));
-    if (!toggle) {
-        bars.forEach(bar => bar.classList.add("menu__bar--active"));
-        nav.classList.add("header-nav--active");
-        toggle=true;
-    } else {
-        bars.forEach(bar => bar.classList.remove("menu__bar--active"));
-        nav.classList.remove("header-nav--active");
-        toggle=false;
+  // Remove the no-animation class after the click;
+  bars.forEach((bar) => bar.classList.remove("menu__bar--no-animation"));
+  if (!toggle) {
+    bars.forEach((bar) => bar.classList.add("menu__bar--active"));
+    nav.classList.add("header-nav--active");
+    toggle = true;
+  } else {
+    bars.forEach((bar) => bar.classList.remove("menu__bar--active"));
+    nav.classList.remove("header-nav--active");
+    toggle = false;
+  }
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (toggle) {
+      bars.forEach((bar) => bar.classList.remove("menu__bar--active"));
+      nav.classList.remove("header-nav--active");
+      toggle = false;
     }
-})
-
-links.forEach(link => {
-    link.addEventListener("click", () => {
-        if (toggle) {
-            bars.forEach(bar => bar.classList.remove("menu__bar--active"));
-            nav.classList.remove("header-nav--active");
-            toggle=false;
-        }
-    })
-})
-
+  });
+});
 
 // PROJECTS
 const projects = document.querySelectorAll(".projects-item");
@@ -39,19 +38,20 @@ const projButtons = document.querySelectorAll(".btn-proj");
 
 // Function to select different category of project
 const selectProjects = (category) => {
-    let selectedButton;
-    projButtons.forEach(button => {
-        button.classList.remove("btn-proj--selected");
-        if (button.innerHTML.toLowerCase() === category.toLowerCase()) {
-            selectedButton = button;
-        }
-    })
-    selectedButton.classList.add("btn-proj--selected");
-    projects.forEach(item => {
-        if (item.classList.contains(category) || category === "all works") {
-            item.style.display = "block";
-        } else {
-            item.style.display = "none";
-        }
-    })
-}
+  let selectedButton;
+  for (let button of projButtons) {
+    button.classList.remove("btn-proj--selected");
+    if (button.innerHTML.toLowerCase().trim() === category.toLowerCase()) {
+      selectedButton = button;
+      break;
+    }
+  }
+  selectedButton.classList.add("btn-proj--selected");
+  projects.forEach((item) => {
+    if (item.classList.contains(category) || category === "all works") {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+};
